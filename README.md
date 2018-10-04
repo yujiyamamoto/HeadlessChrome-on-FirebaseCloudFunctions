41d8de379f6f9056ac883e4e7212007a263af57f
yuji yamamoto <y.goodspeed@gmail.com>
2018-10-04 20:16:16 +0900

    update

---
+++ README.md
@@ -0,0 +1,17 @@
+
+# puppeteer
+https://github.com/GoogleChrome/puppeteer
+
+# Introducing headless Chrome support in Cloud Functions and App Engine
+https://cloud.google.com/blog/products/gcp/introducing-headless-chrome-support-in-cloud-functions-and-app-engine
+
+# Set timeout and memory allocation
+https://firebase.google.com/docs/functions/manage-functions?hl=en#set_timeout_and_memory_allocation
+
+
+you need firebase-tools 4.0.0 or higher
+```
+$ cd functions
+$ npm install
+$ functions deploy --only functions:example --project [FirebaseProjectID]
+```
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,29 @@
 const functions = require('firebase-functions');
+const puppeteer = require('puppeteer');
 
+'use strict';
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const runtimeOpts = {
+  timeoutSeconds: 300,
+  memory: '1GB'
+}
+
+exports.example = functions
+  .runWith(runtimeOpts)
+  .https.onRequest(async(req, res) => {
+    const url = "https://example.com";
+
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox']
+    });
+    const page = await browser.newPage();
+    await page.goto(url);
+    res.send(await page.title());
+    await browser.close();
+  });
